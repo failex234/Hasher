5d8cb267803859f4f7bf5c866265ec1039d3bead
Felix Naumann <felix@emilnaumann.de>
2018-07-15 17:48:08 +0200

    inital commit

---
+++ Hasher/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Hasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fd = new System.Windows.Forms.OpenFileDialog();
+            this.tb_file = new System.Windows.Forms.TextBox();
+            this.btn_browse = new System.Windows.Forms.Button();
+            this.lbl_selectfile = new System.Windows.Forms.Label();
+            this.tb_string = new System.Windows.Forms.TextBox();
+            this.lbl_or = new System.Windows.Forms.Label();
+            this.rb_md5 = new System.Windows.Forms.RadioButton();
+            this.rb_sha256 = new System.Windows.Forms.RadioButton();
+            this.rb_sha512 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_hash = new System.Windows.Forms.Button();
+            this.tb_hash = new System.Windows.Forms.TextBox();
+            this.btn_browse2 = new System.Windows.Forms.Button();
+            this.cb_compare = new System.Windows.Forms.CheckBox();
+            this.fd2 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_file
+            // 
+            this.tb_file.Location = new System.Drawing.Point(12, 29);
+            this.tb_file.Name = "tb_file";
+            this.tb_file.Size = new System.Drawing.Size(535, 20);
+            this.tb_file.TabIndex = 0;
+            // 
+            // btn_browse
+            // 
+            this.btn_browse.Location = new System.Drawing.Point(554, 28);
+            this.btn_browse.Name = "btn_browse";
+            this.btn_browse.Size = new System.Drawing.Size(30, 20);
+            this.btn_browse.TabIndex = 1;
+            this.btn_browse.Text = "...";
+            this.btn_browse.UseVisualStyleBackColor = true;
+            this.btn_browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_selectfile
+            // 
+            this.lbl_selectfile.AutoSize = true;
+            this.lbl_selectfile.Location = new System.Drawing.Point(250, 9);
+            this.lbl_selectfile.Name = "lbl_selectfile";
+            this.lbl_selectfile.Size = new System.Drawing.Size(56, 13);
+            this.lbl_selectfile.TabIndex = 2;
+            this.lbl_selectfile.Text = "Select File";
+            // 
+            // tb_string
+            // 
+            this.tb_string.Location = new System.Drawing.Point(12, 72);
+            this.tb_string.Name = "tb_string";
+            this.tb_string.Size = new System.Drawing.Size(535, 20);
+            this.tb_string.TabIndex = 3;
+            // 
+            // lbl_or
+            // 
+            this.lbl_or.AutoSize = true;
+            this.lbl_or.Location = new System.Drawing.Point(238, 56);
+            this.lbl_or.Name = "lbl_or";
+            this.lbl_or.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lbl_or.Size = new System.Drawing.Size(82, 13);
+            this.lbl_or.TabIndex = 4;
+            this.lbl_or.Text = "or enter a String";
+            this.lbl_or.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // rb_md5
+            // 
+            this.rb_md5.AutoSize = true;
+            this.rb_md5.Location = new System.Drawing.Point(183, 138);
+            this.rb_md5.Name = "rb_md5";
+            this.rb_md5.Size = new System.Drawing.Size(48, 17);
+            this.rb_md5.TabIndex = 5;
+            this.rb_md5.TabStop = true;
+            this.rb_md5.Text = "MD5";
+            this.rb_md5.UseVisualStyleBackColor = true;
+            // 
+            // rb_sha256
+            // 
+            this.rb_sha256.AutoSize = true;
+            this.rb_sha256.Location = new System.Drawing.Point(237, 138);
+            this.rb_sha256.Name = "rb_sha256";
+            this.rb_sha256.Size = new System.Drawing.Size(68, 17);
+            this.rb_sha256.TabIndex = 6;
+            this.rb_sha256.TabStop = true;
+            this.rb_sha256.Text = "SHA-256";
+            this.rb_sha256.UseVisualStyleBackColor = true;
+            // 
+            // rb_sha512
+            // 
+            this.rb_sha512.AutoSize = true;
+            this.rb_sha512.Location = new System.Drawing.Point(311, 138);
+            this.rb_sha512.Name = "rb_sha512";
+            this.rb_sha512.Size = new System.Drawing.Size(68, 17);
+            this.rb_sha512.TabIndex = 7;
+            this.rb_sha512.TabStop = true;
+            this.rb_sha512.Text = "SHA-512";
+            this.rb_sha512.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(250, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Algorithm";
+            // 
+            // btn_hash
+            // 
+            this.btn_hash.Location = new System.Drawing.Point(224, 161);
+            this.btn_hash.Name = "btn_hash";
+            this.btn_hash.Size = new System.Drawing.Size(96, 31);
+            this.btn_hash.TabIndex = 10;
+            this.btn_hash.Text = "Calculate";
+            this.btn_hash.UseVisualStyleBackColor = true;
+            this.btn_hash.Click += new System.EventHandler(this.btn_hash_Click);
+            // 
+            // tb_hash
+            // 
+            this.tb_hash.Location = new System.Drawing.Point(12, 222);
+            this.tb_hash.Name = "tb_hash";
+            this.tb_hash.Size = new System.Drawing.Size(534, 20);
+            this.tb_hash.TabIndex = 11;
+            // 
+            // btn_browse2
+            // 
+            this.btn_browse2.Enabled = false;
+            this.btn_browse2.Location = new System.Drawing.Point(554, 72);
+            this.btn_browse2.Name = "btn_browse2";
+            this.btn_browse2.Size = new System.Drawing.Size(30, 20);
+            this.btn_browse2.TabIndex = 12;
+            this.btn_browse2.Text = "...";
+            this.btn_browse2.UseVisualStyleBackColor = true;
+            this.btn_browse2.Visible = false;
+            this.btn_browse2.Click += new System.EventHandler(this.btn_browse2_Click);
+            // 
+            // cb_compare
+            // 
+            this.cb_compare.AutoSize = true;
+            this.cb_compare.Location = new System.Drawing.Point(13, 99);
+            this.cb_compare.Name = "cb_compare";
+            this.cb_compare.Size = new System.Drawing.Size(68, 17);
+            this.cb_compare.TabIndex = 13;
+            this.cb_compare.Text = "Compare";
+            this.cb_compare.UseVisualStyleBackColor = true;
+            this.cb_compare.CheckedChanged += new System.EventHandler(this.cb_compare_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(259, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(596, 253);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cb_compare);
+            this.Controls.Add(this.btn_browse2);
+            this.Controls.Add(this.tb_hash);
+            this.Controls.Add(this.btn_hash);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rb_sha512);
+            this.Controls.Add(this.rb_sha256);
+            this.Controls.Add(this.rb_md5);
+            this.Controls.Add(this.lbl_or);
+            this.Controls.Add(this.tb_string);
+            this.Controls.Add(this.lbl_selectfile);
+            this.Controls.Add(this.btn_browse);
+            this.Controls.Add(this.tb_file);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Hasher";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog fd;
+        private System.Windows.Forms.TextBox tb_file;
+        private System.Windows.Forms.Button btn_browse;
+        private System.Windows.Forms.Label lbl_selectfile;
+        private System.Windows.Forms.TextBox tb_string;
+        private System.Windows.Forms.Label lbl_or;
+        private System.Windows.Forms.RadioButton rb_md5;
+        private System.Windows.Forms.RadioButton rb_sha256;
+        private System.Windows.Forms.RadioButton rb_sha512;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_hash;
+        private System.Windows.Forms.TextBox tb_hash;
+        private System.Windows.Forms.Button btn_browse2;
+        private System.Windows.Forms.CheckBox cb_compare;
+        private System.Windows.Forms.OpenFileDialog fd2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Hasher/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace Hasher
+{
+    //TODO formatting of lbl_or when in compare mode
+    public partial class Form1 : Form
+    {
+
+        Boolean compare = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Open the file dialog and set the value of the textbox to the filepath of the selected file
+            DialogResult result = fd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                tb_file.Text = fd.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rb_md5.Select();
+        }
+
+        public static Stream GenerateStreamFromString(String s)
+        {
+            //Write String into a new Stream using a StreamWriter
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        /**
+        * type = 0 | File
+        * type = 1 | String
+        */
+        string CalculateHash(String s, int type)
+        {
+            HashAlgorithm hash;
+
+            //Determine HashAlgorithm by looking at what radio button is selected
+            if (rb_md5.Checked) hash = MD5.Create();
+            else if (rb_sha256.Checked) hash = SHA256.Create();
+            else if (rb_sha512.Checked) hash = SHA512.Create();
+            else
+            {
+                MessageBox.Show("Invalid state", "You have selected an invalid hash algorithm!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return "ERROR";
+            }
+
+            using (var stream = type == 0 ? File.OpenRead(s) : GenerateStreamFromString(s))
+            {
+                var calcedhash = hash.ComputeHash(stream);
+                return BitConverter.ToString(calcedhash).Replace("-", "").ToLowerInvariant();
+            }
+
+        }
+
+        private void btn_hash_Click(object sender, EventArgs e)
+        {
+            //Check if compare mode is on
+            if (!compare)
+            {
+                //Check if all needed textboxes are filled / have legal values
+                String hash = "";
+                if (tb_file.Text == "")
+                {
+                    if (tb_string.Text == "")
+                    {
+                        MessageBox.Show("You need to select a file or enter a String", "Input missing!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    else
+                    {
+                        hash = CalculateHash(tb_string.Text, 1);
+                    }
+                }
+                else
+                {
+                    //Calculate Hash if input file exists
+                    if (File.Exists(tb_file.Text))
+                    {
+                        hash = CalculateHash(tb_file.Text, 0);
+                    }
+                    else
+                    {
+                        MessageBox.Show("The selected file doesn't exist!", "File not found!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+
+                tb_hash.Text = hash;
+            }
+            else
+            {
+                //Check if both textboxes are not empty
+                if (tb_file.Text == "" || tb_string.Text == "")
+                {
+                    MessageBox.Show("Both Textboxes need to contain a value to compare", "Empty Textbox", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (!File.Exists(tb_file.Text))
+                {
+                    MessageBox.Show("The selected file doesn't exist!", "File not found!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                //Make sure that the value of the second textbox is either a file or a hash (Can be made more precise)
+                if (!File.Exists(tb_string.Text) && (tb_string.Text.Length != 32 || tb_string.Text.Length != 64 || tb_string.Text.Length != 128))
+                {
+                    MessageBox.Show("Input is neither a file nor a hash, please check input!", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (File.Exists(tb_string.Text))
+                {
+                    try
+                    {
+                        //Read sum from file. Most sum files have the layout "hashsum filename"
+                        //So we want to isolate the hashsum from the filename
+                        StreamReader sr = new StreamReader(tb_string.Text);
+                        String hashfromfile = sr.ReadLine().Split(' ')[0];
+                        sr.Close();
+
+                        //Just compare both hashes
+                        if (CalculateHash(tb_file.Text, 0).Equals(hashfromfile))
+                        {
+                            tb_hash.Text = "hashes ARE the same";
+                        }
+                        else
+                        {
+                            tb_hash.Text = "hases are NOT the same";
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Error while reading file", "Can't read file", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+                else
+                {
+                    String secondhash = tb_string.Text;
+
+                    //Just compare both hashes
+                    if (CalculateHash(tb_file.Text, 0).Equals(secondhash))
+                    {
+                        tb_hash.Text = "hashes ARE the same";
+                    }
+                    else
+                    {
+                        tb_hash.Text = "hases are NOT the same";
+                    }
+                }
+            }
+        }
+
+        private void cb_compare_CheckedChanged(object sender, EventArgs e)
+        {
+            //Change button and label values to make it clear that compare mode is turned on
+            if (cb_compare.Checked)
+            {
+                btn_browse2.Visible = true;
+                btn_browse2.Enabled = true;
+                lbl_or.Text = "and enter a Hash / browse a hash file to compare to";
+                compare = true;
+                btn_hash.Text = "Compare";
+            }
+            else
+            {
+                btn_browse2.Visible = false;
+                btn_browse2.Enabled = false;
+                lbl_or.Text = "or enter a String";
+                compare = false;
+                btn_hash.Text = "Calculate";
+            }
+        }
+
+        private void btn_browse2_Click(object sender, EventArgs e)
+        {
+            //Open the file dialog and set the value of the textbox to the filepath of the selected file
+            DialogResult result = fd2.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                tb_string.Text = fd2.FileName;
+            }
+        }
+    }
+}
